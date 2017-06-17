bf7e984940ef2693abddfbcbd592bf1588f0bb69
se7enlove157 <se7en.love157@gmail.com>
2017-06-17 20:52:24 +0700

    Update Layout Admin

---
+++ DauGia/DauGia/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DauGia.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
+    }
+}
